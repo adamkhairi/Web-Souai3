41bbb93ea49a26c053773f9105b9d23f4f643bc1
adamkhairi <57996941+adamkhairi@users.noreply.github.com>
2020-05-16 02:12:15 +0000

    db change

--- sway3.sql
+++ sway3.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 15, 2020 at 07:11 AM
+-- Generation Time: May 16, 2020 at 04:08 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.2.30
 
@@ -46,24 +46,49 @@ INSERT INTO `benevole` (`idbenevole`, `nombenevole`, `prenombenevole`, `mailbene
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `cours`
+--
+
+CREATE TABLE `cours` (
+  `idcours` int(11) NOT NULL,
+  `nomcours` varchar(255) NOT NULL,
+  `idmatiere` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `cours`
+--
+
+INSERT INTO `cours` (`idcours`, `nomcours`, `idmatiere`) VALUES
+(1, 'algebre', 1),
+(2, 'sss', 1),
+(3, 'dddd', 2),
+(4, 'rr', 2),
+(6, 'rr', 2);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `demande`
 --
 
 CREATE TABLE `demande` (
-  `idcours` int(11) NOT NULL DEFAULT 0,
-  `matiere` text NOT NULL,
-  `cours` text NOT NULL,
+  `iddemande` int(11) NOT NULL,
   `description` text NOT NULL,
-  `idetudiantc` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `idetudiantc` int(11) DEFAULT NULL,
+  `idcours` int(11) NOT NULL,
+  `idniveau` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `demande`
 --
 
-INSERT INTO `demande` (`idcours`, `matiere`, `cours`, `description`, `idetudiantc`) VALUES
-(0, '2a', '2a', '2a', 8),
-(1, 'aa', 'aa', 'aa', 8);
+INSERT INTO `demande` (`iddemande`, `description`, `idetudiantc`, `idcours`, `idniveau`) VALUES
+(1, ' sssss', 8, 0, 0),
+(2, 'cxxx', 8, 0, 0),
+(3, 'hhhh', 8, 0, 0),
+(4, 'xxxx', 8, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -86,12 +111,54 @@ CREATE TABLE `etudiant` (
 --
 
 INSERT INTO `etudiant` (`idetudiant`, `nometudiant`, `prenometudiant`, `niveauscolaire`, `filiere`, `mailetudiant`, `passwordetudiant`) VALUES
-(3, '11', '11', '11', '1', '11@1.1', '6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b'),
+(3, '11', '11', '1', '1', '11@1.1', '6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b'),
 (4, '22', '2', '2', '2', '22@2.2', 'd4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35'),
 (5, '22', '2', '2', '2', '2@2.2', 'd4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35'),
-(6, '44', '44', '44', '44', '44@4.4', '71ee45a3c0db9a9865f7313dd3372cf60dca6479d46261f3542eb9346e4a04d6'),
-(7, '33', '33', '33', '33', '33@3.3', 'c6f3ac57944a531490cd39902d0f777715fd005efac9a30622d5f5205e7f6894'),
-(8, 'a', 'a', 'a', 'a', 'a@a.a', 'ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb');
+(6, '44', '44', '2', '44', '44@4.4', '71ee45a3c0db9a9865f7313dd3372cf60dca6479d46261f3542eb9346e4a04d6'),
+(7, '33', '33', '1', '33', '33@3.3', 'c6f3ac57944a531490cd39902d0f777715fd005efac9a30622d5f5205e7f6894'),
+(8, 'a', 'a', '2', 'a', 'a@a.a', 'ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb'),
+(9, 'e', 'e', '2', 'e', 'e@e.e', '3f79bb7b435b05321651daefd374cdc681dc06faa65e374e38337b88ca046dea');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matiere`
+--
+
+CREATE TABLE `matiere` (
+  `idmatiere` int(11) NOT NULL,
+  `nommatiere` varchar(255) NOT NULL,
+  `idniveau` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `matiere`
+--
+
+INSERT INTO `matiere` (`idmatiere`, `nommatiere`, `idniveau`) VALUES
+(1, 'math', 1),
+(2, 'pc', 2),
+(3, 'fr', 1),
+(4, 'ar', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `niveau`
+--
+
+CREATE TABLE `niveau` (
+  `idniveau` int(11) NOT NULL,
+  `niveau` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `niveau`
+--
+
+INSERT INTO `niveau` (`idniveau`, `niveau`) VALUES
+(1, '2eme bac'),
+(2, '1er bac');
 
 --
 -- Indexes for dumped tables
@@ -104,10 +171,16 @@ ALTER TABLE `benevole`
   ADD PRIMARY KEY (`idbenevole`);
 
 --
+-- Indexes for table `cours`
+--
+ALTER TABLE `cours`
+  ADD PRIMARY KEY (`idcours`);
+
+--
 -- Indexes for table `demande`
 --
 ALTER TABLE `demande`
-  ADD PRIMARY KEY (`idcours`),
+  ADD PRIMARY KEY (`iddemande`),
   ADD KEY `idetudiantc` (`idetudiantc`);
 
 --
@@ -115,6 +188,18 @@ ALTER TABLE `demande`
 --
 ALTER TABLE `etudiant`
   ADD PRIMARY KEY (`idetudiant`);
+
+--
+-- Indexes for table `matiere`
+--
+ALTER TABLE `matiere`
+  ADD PRIMARY KEY (`idmatiere`);
+
+--
+-- Indexes for table `niveau`
+--
+ALTER TABLE `niveau`
+  ADD PRIMARY KEY (`idniveau`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -127,10 +212,34 @@ ALTER TABLE `benevole`
   MODIFY `idbenevole` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT for table `cours`
+--
+ALTER TABLE `cours`
+  MODIFY `idcours` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `demande`
+--
+ALTER TABLE `demande`
+  MODIFY `iddemande` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT for table `etudiant`
 --
 ALTER TABLE `etudiant`
-  MODIFY `idetudiant` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `idetudiant` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `matiere`
+--
+ALTER TABLE `matiere`
+  MODIFY `idmatiere` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `niveau`
+--
+ALTER TABLE `niveau`
+  MODIFY `idniveau` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
