a96fc1c8d769e336773c476c4ff9c1fa29a59464
khouilid <57531206+khouilid@users.noreply.github.com>
2020-05-17 04:56:49 +0000

    check if the email existe

--- forget_the_password.php
+++ forget_the_password.php
@@ -11,11 +11,32 @@ include("navbar.php");
 
 <div class="signin-form">
     <h2 class="form-title">Mode passe Oublie</h2>
-    <form method="POST" action="forgetPasseword.php" class="register-form" id="login-form">
+    <form method="POST" action="<?php echo $_SERVER['PHP_SELF'];?>" class="register-form" id="login-form">
         <div class="form-group">
             <label for="your_email"><i class="zmdi zmdi-account material-icons-name"></i></label>
             <input type="email" name="email" id="your_email" placeholder="Votre Email"/>
         </div>
+        <?php 
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    $email = $_POST['email'];
+                    if(!empty($email)){
+                        $sql = "SELECT * FROM `etudiant` WHERE mailetudiant = '". $email ."';";
+                        $result = $conn->query($sql);
+                        $reponse = mysqli_fetch_array($result);
+                        if(!empty($reponse['mailetudiant'])){
+                            //TODO Send email to Client
+                        // header("location: resetNewPassword.php");
+                        echo"<p class ='text-success m-4'>vérifiez votre email!</p>";
+                        }
+                        else{
+                            echo"<p class ='text-danger m-4'>cet email n'existe pas!</p>";
+                        }
+                    } else{
+                        header("location: index.php");
+                    }
+                }
+        ?>
+
         <input type="text" name="userType" hidden id="userType" value="">
         <div class="form-group form-button">
             <input type="submit" name="send_email" id="signin" class="form-submit" value="Connexion"/>
--- ressetthepassword.php
+++ ressetthepassword.php
@@ -3,16 +3,25 @@ require("connexion.php");
 $email = $_POST['email'];
 $password1 = $_POST['password1'];
 $password2 = $_POST['password2'];
-
-
-if ($password1 == $password2) {
-    $sql = "UPDATE `etudiant` SET `passwordetudiant` = '" . hash('sha256', $password1) . "' WHERE `mailetudiant` = '". $email ."';";
-    $req = mysqli_query($conn,$sql);
-}else {
-    echo "<script>alert('blbla')</script>";
+if(!empty($email)){
+    $sql = "SELECT * FROM `etudiant` WHERE mailetudiant = '". $email ."';";
+    $result = $conn->query($sql);
+    $reponse = mysqli_fetch_array($result);
+    if(!empty($reponse['mailetudiant'])){
+        if ($password1 == $password2) {
+            $sql = "UPDATE `etudiant` SET `passwordetudiant` = '" . hash('sha256', $password1) . "' WHERE `mailetudiant` = '". $email ."';";
+            $req = mysqli_query($conn,$sql);
+            header("location: index.php");
+            echo "<script>alert('Done')</script>";
+        }else {
+            
+            header("location: resetthepassword.php");
+        }
+      
+    }
+    else{
+        header("location: forget_the_password.php");
+      
+    }
 }
-
-
-
-
 ?>
